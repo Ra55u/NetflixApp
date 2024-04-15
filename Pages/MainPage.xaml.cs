905ccb8eed2ddd8b060f16089ab9d06ddc298665
Opilane TTHK <opilane@tthk.ee>
2024-04-15 10:44:08 +0300

    commit

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -4,32 +4,18 @@ namespace NetflixApp.Pages;
 
 public partial class MainPage : ContentPage
 {
-    private readonly HomeViewModel _homeViewModel;
-    public MainPage(HomeViewModel homeViewModel)
-    {
-        InitializeComponent();
-        _homeViewModel = homeViewModel;
-        BindingContext = _homeViewModel;
-    }
+	private readonly HomeViewModel _homeViewModel;
+	public MainPage(HomeViewModel homeViewModel)
+	{
+		InitializeComponent();
+		_homeViewModel = homeViewModel;
+		BindingContext = _homeViewModel;
+	}
 
     protected async override void OnAppearing()
     {
         base.OnAppearing();
-        await _homeViewModel.InitializeAsync();
-    }
-
-    private void MovieRow_MediaSelected(object sender, Controls.MediaSelectEventArgs e)
-    {
-        _homeViewModel.SelectMediaCommand.Execute(e.Media);
-    }
-
-    private void MovieInfoBox_Closed(object sender, EventArgs e)
-    {
-        _homeViewModel.SelectMediaCommand.Execute(null);
-    }
-
-    private async void CategoriesMenu_Tapped(object sender, TappedEventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(CategoriesPage));
+		await _homeViewModel.InitializeAsync();
     }
 }
+
